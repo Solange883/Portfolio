1dc496f27dffe1576e25b282f7bdac3c1825551e
Solange883 <soldesages88@gmail.com>
2025-01-13 00:40:45 +0000

    2eme changement

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const navLinks=document.querySelector('.nav-links');
 menuHamburger.onclick=() =>{ 
     navLinks.classList.toggle('mobile-menu');
 };
+
+
+
+
+
+
+
+
+
+const sr=ScrollReveal({
+    origin:'top',
+    distance:'80px',
+    duration:2000?
+    reset:true
+})
+
+sr.reveal('.home_title',{})
+sr.reveal('.button',{delay:200})
